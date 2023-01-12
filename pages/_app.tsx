41e41bea4b71d3478d74cb3b9ac62228b0f29583
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2023-01-12 20:22:32 +0100

    Add sakura.css and prismjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import Prism from "prismjs";
+import React from 'react';
+
+import 'sakura.css/css/normalize.css';
+import 'sakura.css/css/sakura.css';
+import 'prismjs/themes/prism-coy.css';
+
+require("prismjs/components/prism-javascript");
+require("prismjs/components/prism-css");
+require("prismjs/components/prism-jsx");
+require("prismjs/components/prism-csharp");
+require("prismjs/components/prism-python");
 
 export default function App({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    Prism.highlightAll();
+  }, []);
+
   return (
     <>
       <Head>
